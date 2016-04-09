f0491f560a37fa9f609876b7e0237c4c652c3877
Daniel Lorenz <daniel.lorenz@extco.de>
2016-04-09 02:16:39 +0200

    [FIX] remove spaces before class comment

--- Classes/Domain/Model/Product/AbstractProduct.php
+++ Classes/Domain/Model/Product/AbstractProduct.php
@@ -2,18 +2,18 @@
 
 namespace Extcode\Cart\Domain\Model\Product;
 
-    /**
-     * This file is part of the TYPO3 CMS project.
-     *
-     * It is free software; you can redistribute it and/or modify it under
-     * the terms of the GNU General Public License, either version 2
-     * of the License, or any later version.
-     *
-     * For the full copyright and license information, please read the
-     * LICENSE.txt file that was distributed with this source code.
-     *
-     * The TYPO3 project - inspiring people to share!
-     */
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
 
 /**
  * Product AbstractProduct Model
