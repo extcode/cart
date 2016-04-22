d4a768a236ee1d8994c407b7b9cc52422e3b7b9d
Daniel Lorenz <daniel.lorenz@extco.de>
2016-04-22 11:17:20 +0200

    [FIX] add missing versioning database fields for CartPaypal

--- ext_tables.sql
+++ ext_tables.sql
@@ -803,6 +803,18 @@ CREATE TABLE tx_cart_domain_model_cart (
     cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
     deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
 
+    t3ver_oid int(11) DEFAULT '0' NOT NULL,
+    t3ver_id int(11) DEFAULT '0' NOT NULL,
+    t3ver_wsid int(11) DEFAULT '0' NOT NULL,
+    t3ver_label varchar(255) DEFAULT '' NOT NULL,
+    t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
+    t3ver_stage int(11) DEFAULT '0' NOT NULL,
+    t3ver_count int(11) DEFAULT '0' NOT NULL,
+    t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+    t3ver_move_id int(11) DEFAULT '0' NOT NULL,
+
+    t3_origuid int(11) DEFAULT '0' NOT NULL,
+
     PRIMARY KEY (uid),
     KEY parent (pid),
 
