5436e647e21666e45124086c609079a0531b2102
Daniel Lorenz <daniel.lorenz@extco.de>
2018-11-25 21:16:12 +0100

    [TASK] cleanup ext_tables.php
    Resolves: #216

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -2,13 +2,26 @@
 defined('TYPO3_MODE') or die();
 
 call_user_func(function () {
-    $_LLL = 'LLL:' . 'EXT:cart/Resources/Private/Language/locallang_db.xlf:';
+    $_LLL_be = 'LLL:EXT:cart/Resources/Private/Language/locallang_be.xlf:';
+    $_LLL_db = 'LLL:EXT:cart/Resources/Private/Language/locallang_db.xlf:';
 
     $GLOBALS['TCA']['pages']['columns']['doktype']['config']['items'][] = [
-        $_LLL . 'pages.doktype.181',
+        $_LLL_db . 'pages.doktype.181',
         181,
         'apps-pagetree-page-cart-cart'
     ];
+    $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [
+        $_LLL_be . 'tcarecords-pages-contains.cart_coupons',
+        'coupons',
+        'apps-pagetree-folder-cart-coupons'
+    ];
+    $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [
+        $_LLL_be . 'tcarecords-pages-contains.cart_orders',
+        'orders',
+        'apps-pagetree-folder-cart-orders'
+    ];
 
     $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes'][181] = 'apps-pagetree-page-cart-cart';
+    $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-coupons'] = 'apps-pagetree-folder-cart-coupons';
+    $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-orders'] = 'apps-pagetree-folder-cart-orders';
 });
---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,39 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+call_user_func(function () {
+    $_LLL_db = 'LLL:EXT:cart/Resources/Private/Language/locallang_db.xlf:';
+
+    /**
+     * Register Frontend Plugins
+     */
+    $pluginNames = [
+        'MiniCart',
+        'Cart',
+        'Currency',
+        'Order',
+    ];
+
+    foreach ($pluginNames as $pluginName) {
+        $pluginSignature = 'cart_' . strtolower($pluginName);
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            'Extcode.cart',
+            $pluginName,
+            $_LLL_db . 'tx_cart.plugin.' . strtolower(preg_replace('/[A-Z]/', '_$0', lcfirst($pluginName)))
+        );
+        $TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key';
+
+        $flexFormPath = 'EXT:cart/Configuration/FlexForms/' . $pluginName . 'Plugin.xml';
+        if (file_exists(\TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName($flexFormPath))) {
+            $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+                $pluginSignature,
+                'FILE:' . $flexFormPath
+            );
+        }
+    }
+
+    $TCA['tt_content']['types']['list']['subtypes_excludelist']['cart_minicart'] = 'select_key,pages,recursive';
+    $TCA['tt_content']['types']['list']['subtypes_excludelist']['cart_currency'] = 'select_key,pages,recursive';
+    $TCA['tt_content']['types']['list']['subtypes_excludelist']['cart_flexproduct'] = 'select_key,pages,recursive';
+});
