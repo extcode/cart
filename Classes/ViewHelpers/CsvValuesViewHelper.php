a8445d80c19e8c990de0be721c5ddd7c271603f5
Daniel Gohlke <daniel.gohlke@extco.de>
2019-04-08 08:14:23 +0200

    [BUGFIX] move export template and fix ViewHelper
    Resolves: #245

--- Classes/ViewHelpers/CsvValuesViewHelper.php
+++ Classes/ViewHelpers/CsvValuesViewHelper.php
@@ -23,6 +23,14 @@ use TYPO3\CMS\Core\Utility\CsvUtility;
  */
 class CsvValuesViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
 {
+
+    /**
+     * Output is escaped already. We must not escape children, to avoid double encoding.
+     *
+     * @var bool
+     */
+    protected $escapeOutput = false;
+
     /**
      * Format OrderItem to CSV format
      *
@@ -36,12 +44,15 @@ class CsvValuesViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewH
     {
         $orderItemArr = [];
 
+        $orderItemArr[] = $orderItem->getOrderNumber();
+        $orderItemArr[] = $orderItem->getOrderDate() ? $orderItem->getOrderDate()->format('d.m.Y') : '';
+        $orderItemArr[] = $orderItem->getInvoiceNumber();
+        $orderItemArr[] = $orderItem->getInvoiceDate() ? $orderItem->getInvoiceDate()->format('d.m.Y') : '';
+
         $orderItemArr[] = $orderItem->getBillingAddress()->getSalutation();
         $orderItemArr[] = $orderItem->getBillingAddress()->getTitle();
         $orderItemArr[] = $orderItem->getBillingAddress()->getFirstName();
         $orderItemArr[] = $orderItem->getBillingAddress()->getLastName();
-        $orderItemArr[] = $orderItem->getOrderNumber();
-        $orderItemArr[] = $orderItem->getInvoiceNumber();
 
         return CsvUtility::csvValues($orderItemArr, $delim, $quote);
     }
