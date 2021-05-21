733dc590b958436b208f2667938cb03a1604e6cc
Daniel Gohlke <daniel.gohlke@extco.de>
2021-05-21 10:05:13 +0200

    [BUGFIX] Change Comparison Operator in Availability Check
    - add some tests
    
    Related: #324

--- Tests/Unit/Domain/Model/Cart/ServiceTest.php
+++ Tests/Unit/Domain/Model/Cart/ServiceTest.php
@@ -24,12 +24,27 @@ class ServiceTest extends UnitTestCase
     protected $config = [];
 
     /**
+     * @var array
+     */
+    protected $taxClasses = [];
+
+    /**
      * @var \Extcode\Cart\Domain\Model\Cart\Service
      */
     protected $service;
 
     public function setUp(): void
     {
+        $this->normalTaxClass = new \Extcode\Cart\Domain\Model\Cart\TaxClass(1, '19', 0.19, 'Normal');
+        $this->reducedTaxClass = new \Extcode\Cart\Domain\Model\Cart\TaxClass(2, '7%', 0.07, 'Reduced');
+        $this->freeTaxClass = new \Extcode\Cart\Domain\Model\Cart\TaxClass(3, '0%', 0.00, 'Free');
+
+        $this->taxClasses = [
+            1 => $this->normalTaxClass,
+            2 => $this->reducedTaxClass,
+            3 => $this->freeTaxClass
+        ];
+
         $this->config = [
             'title' => 'Standard',
             'extra' => 0.00,
@@ -62,6 +77,139 @@ class ServiceTest extends UnitTestCase
         $this->assertSame(
             $this->config,
             $this->service->getConfig()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function isAvailableWithoutUntilAvailableConfigurationReturnsTrue()
+    {
+        $this->assertTrue(
+            $this->service->isAvailable()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function isAvailableWithCartGrossInRangeReturnsTrue()
+    {
+        $config = [
+            'title' => 'Standard',
+            'extra' => 0.00,
+            'taxClassId' => 1,
+            'status' => 'open',
+            'available' => [
+                'from' => 20.00,
+                'until' => 100.00
+            ]
+        ];
+
+        $service = $this->getMockBuilder(\Extcode\Cart\Domain\Model\Cart\Service::class)
+            ->setMethods(['calcAll'])
+            ->setConstructorArgs([$this->id, $config])
+            ->getMock();
+        $service->expects($this->any())->method('calcAll')->will($this->returnValue(null));
+
+        $cart1 = $this->getMockBuilder(\Extcode\Cart\Domain\Model\Cart\Cart::class)
+            ->setMethods(['getGross'])
+            ->setConstructorArgs([$this->taxClasses])
+            ->getMock();
+        $cart1->expects($this->any())->method('getGross')->will($this->returnValue(20.00));
+        $service->setCart($cart1);
+        $this->assertTrue(
+            $service->isAvailable()
+        );
+
+        $cart2 = $this->getMockBuilder(\Extcode\Cart\Domain\Model\Cart\Cart::class)
+            ->setMethods(['getGross'])
+            ->setConstructorArgs([$this->taxClasses])
+            ->getMock();
+        $cart2->expects($this->any())->method('getGross')->will($this->returnValue(50.00));
+        $service->setCart($cart2);
+        $this->assertTrue(
+            $service->isAvailable()
+        );
+
+        $cart3 = $this->getMockBuilder(\Extcode\Cart\Domain\Model\Cart\Cart::class)
+            ->setMethods(['getGross'])
+            ->setConstructorArgs([$this->taxClasses])
+            ->getMock();
+        $cart3->expects($this->any())->method('getGross')->will($this->returnValue(100.00));
+        $service->setCart($cart3);
+        $this->assertTrue(
+            $service->isAvailable()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function isAvailableWithCartGrossBelowRangeReturnsFalse()
+    {
+        $config = [
+            'title' => 'Standard',
+            'extra' => 0.00,
+            'taxClassId' => 1,
+            'status' => 'open',
+            'available' => [
+                'from' => 20.00,
+                'until' => 100.00
+            ]
+        ];
+
+        $service = $this->getMockBuilder(\Extcode\Cart\Domain\Model\Cart\Service::class)
+            ->setMethods(['calcAll'])
+            ->setConstructorArgs([$this->id, $config])
+            ->getMock();
+        $service->expects($this->any())->method('calcAll')->will($this->returnValue(null));
+
+        $cart = $this->getMockBuilder(\Extcode\Cart\Domain\Model\Cart\Cart::class)
+            ->setMethods(['getGross'])
+            ->setConstructorArgs([$this->taxClasses])
+            ->getMock();
+        $cart->expects($this->any())->method('getGross')->will($this->returnValue(19.99));
+
+        $service->setCart($cart);
+
+        $this->assertFalse(
+            $service->isAvailable()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function isAvailableWithCartGrossAboveRangeReturnsFalse()
+    {
+        $config = [
+            'title' => 'Standard',
+            'extra' => 0.00,
+            'taxClassId' => 1,
+            'status' => 'open',
+            'available' => [
+                'from' => 20.00,
+                'until' => 100.00
+            ]
+        ];
+
+        $service = $this->getMockBuilder(\Extcode\Cart\Domain\Model\Cart\Service::class)
+            ->setMethods(['calcAll'])
+            ->setConstructorArgs([$this->id, $config])
+            ->getMock();
+        $service->expects($this->any())->method('calcAll')->will($this->returnValue(null));
+
+        $cart = $this->getMockBuilder(\Extcode\Cart\Domain\Model\Cart\Cart::class)
+            ->setMethods(['getGross'])
+            ->setConstructorArgs([$this->taxClasses])
+            ->getMock();
+        $cart->expects($this->any())->method('getGross')->will($this->returnValue(100.01));
+
+        $service->setCart($cart);
+
+        $this->assertFalse(
+            $service->isAvailable()
         );
     }
 }
