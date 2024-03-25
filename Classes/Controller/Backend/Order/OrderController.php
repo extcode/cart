extcode/cart

commit 9845b71868aa15877e0523fee8519990ab2187b5

[TASK] Remove v12 deprecations (#460)
* Do no longer use `GeneralUtility::_GET(), see https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/11.0/Deprecation-92947-DeprecateTYPO3_MODEAndTYPO3_REQUESTTYPEConstants.html
* Do no longer use `AbstractMessage`, see https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Breaking-97787-AbstractMessageGetSeverityReturnsContextualFeedbackSeverity.html

Related: #433

Classes/Controller/Backend/Order/OrderController.php
@@ -19,10 +19,9 @@ use Psr\Http\Message\ResponseInterface;
 use TYPO3\CMS\Backend\Template\ModuleTemplate;
 use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
 use TYPO3\CMS\Core\Imaging\IconFactory;
-use TYPO3\CMS\Core\Messaging\AbstractMessage;
 use TYPO3\CMS\Core\Pagination\SimplePagination;
+use TYPO3\CMS\Core\Type\ContextualFeedbackSeverity;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3\CMS\Extbase\Annotation\IgnoreValidation;
 use TYPO3\CMS\Extbase\Pagination\QueryResultPaginator;
 use TYPO3\CMS\Extbase\Persistence\Generic\PersistenceManager;
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
@@ -177,7 +176,7 @@ class OrderController extends ActionController
                 'Cart'
             );
 
-            $this->addFlashMessage($msg, '', AbstractMessage::ERROR);
+            $this->addFlashMessage($msg, '', ContextualFeedbackSeverity::ERROR);
         }
 
         $this->redirect('show', 'Backend\Order\Order', null, ['orderItem' => $orderItem]);

Classes/Controller/Order/OrderController.php
@@ -13,8 +13,8 @@ namespace Extcode\Cart\Controller\Order;
 use Extcode\Cart\Domain\Model\Order\Item;
 use Extcode\Cart\Domain\Repository\Order\ItemRepository;
 use Psr\Http\Message\ResponseInterface;
-use TYPO3\CMS\Core\Messaging\AbstractMessage;
 use TYPO3\CMS\Core\Pagination\SimplePagination;
+use TYPO3\CMS\Core\Type\ContextualFeedbackSeverity;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Extbase\Annotation\IgnoreValidation;
 use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
@@ -80,7 +80,7 @@ class OrderController extends ActionController
             $this->addFlashMessage(
                 'Access denied.',
                 '',
-                AbstractMessage::ERROR
+                ContextualFeedbackSeverity::ERROR
             );
             $this->redirect('list');
         }

Classes/Controller/Backend/ActionController.php
@@ -26,7 +26,7 @@ class ActionController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControlle
                 ConfigurationManager::CONFIGURATION_TYPE_FRAMEWORK
             );
 
-        $pageId = (int)(GeneralUtility::_GET('id')) ? GeneralUtility::_GET('id') : 1;
+        $pageId = (int)$this->request->getQueryParams()['id'] ?? 1;
 
         BackendUtility::readPageAccess(
             $pageId,

Classes/Controller/Cart/CouponController.php
@@ -15,7 +15,7 @@ use Extcode\Cart\Domain\Model\Cart\CartCouponInterface;
 use Extcode\Cart\Domain\Model\Coupon;
 use Extcode\Cart\Domain\Repository\CouponRepository;
 use Psr\Http\Message\ResponseInterface;
-use TYPO3\CMS\Core\Messaging\AbstractMessage;
+use TYPO3\CMS\Core\Type\ContextualFeedbackSeverity;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 
@@ -71,7 +71,7 @@ class CouponController extends ActionController
                                 'Cart'
                             ),
                             '',
-                            AbstractMessage::OK,
+                            ContextualFeedbackSeverity::OK,
                             true
                         );
                     }
@@ -82,7 +82,7 @@ class CouponController extends ActionController
                                 'Cart'
                             ),
                             '',
-                            AbstractMessage::WARNING,
+                            ContextualFeedbackSeverity::WARNING,
                             true
                         );
                     }
@@ -93,7 +93,7 @@ class CouponController extends ActionController
                                 'Cart'
                             ),
                             '',
-                            AbstractMessage::WARNING,
+                            ContextualFeedbackSeverity::WARNING,
                             true
                         );
                     }
@@ -104,7 +104,7 @@ class CouponController extends ActionController
                             'Cart'
                         ),
                         '',
-                        AbstractMessage::WARNING,
+                        ContextualFeedbackSeverity::WARNING,
                         true
                     );
                 }
@@ -115,7 +115,7 @@ class CouponController extends ActionController
                         'Cart'
                     ),
                     '',
-                    AbstractMessage::WARNING,
+                    ContextualFeedbackSeverity::WARNING,
                     true
                 );
             }
@@ -142,7 +142,7 @@ class CouponController extends ActionController
                         'Cart'
                     ),
                     '',
-                    AbstractMessage::OK,
+                    ContextualFeedbackSeverity::OK,
                     true
                 );
             }
@@ -153,7 +153,7 @@ class CouponController extends ActionController
                         'Cart'
                     ),
                     '',
-                    AbstractMessage::WARNING,
+                    ContextualFeedbackSeverity::WARNING,
                     true
                 );
             }

Classes/Controller/Cart/PaymentController.php
@@ -12,7 +12,7 @@ namespace Extcode\Cart\Controller\Cart;
  */
 
 use Psr\Http\Message\ResponseInterface;
-use TYPO3\CMS\Core\Messaging\AbstractMessage;
+use TYPO3\CMS\Core\Type\ContextualFeedbackSeverity;
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 
 class PaymentController extends ActionController
@@ -37,7 +37,7 @@ class PaymentController extends ActionController
                         'Cart'
                     ),
                     '',
-                    AbstractMessage::ERROR,
+                    ContextualFeedbackSeverity::ERROR,
                     true
                 );
             }

Classes/Controller/Cart/ShippingController.php
@@ -12,7 +12,7 @@ namespace Extcode\Cart\Controller\Cart;
  */
 
 use Psr\Http\Message\ResponseInterface;
-use TYPO3\CMS\Core\Messaging\AbstractMessage;
+use TYPO3\CMS\Core\Type\ContextualFeedbackSeverity;
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 
 class ShippingController extends ActionController
@@ -37,7 +37,7 @@ class ShippingController extends ActionController
                         'Cart'
                     ),
                     '',
-                    AbstractMessage::ERROR,
+                    ContextualFeedbackSeverity::ERROR,
                     true
                 );
             }

Classes/Domain/Finisher/Form/AddToCartFinisher.php
@@ -16,9 +16,9 @@ use Extcode\Cart\Service\SessionHandler;
 use Extcode\Cart\Utility\CartUtility;
 use TYPO3\CMS\Core\Http\PropagateResponseException;
 use TYPO3\CMS\Core\Http\StreamFactory;
-use TYPO3\CMS\Core\Messaging\AbstractMessage;
 use TYPO3\CMS\Core\Messaging\FlashMessage;
 use TYPO3\CMS\Core\Messaging\FlashMessageService;
+use TYPO3\CMS\Core\Type\ContextualFeedbackSeverity;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
 use TYPO3\CMS\Extbase\Service\ExtensionService;
@@ -90,7 +90,7 @@ class AddToCartFinisher extends AbstractFinisher
             $status = '200';
             $messageBody = $this->getStatusMessageBody($formValues, $status);
             $messageTitle = $this->getStatusMessageTitle($formValues);
-            $severity = AbstractMessage::OK;
+            $severity = ContextualFeedbackSeverity::OK;
 
             $pageType = $GLOBALS['TYPO3_REQUEST']->getAttribute('routing')->getPageType();
             if (in_array((int)$pageType, $this->configurations['settings']['jsonResponseForPageTypes'])) {

commit cee9e91cc2e927b5f23d7f361d89e1bb4919c19e

[BUGFIX] Readd import of `IgnoreValidation`
This is a regression bug introduced with
https://github.com/extcode/cart/commit/9845b71868aa15877e0523fee8519990ab2187b5#diff-9c9e93f33fe3d5bebb68c6aa63d43e55541ee3f0a8bf0ab73f57430e86c2276b

Classes/Controller/Backend/Order/OrderController.php
@@ -22,6 +22,7 @@ use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\Pagination\SimplePagination;
 use TYPO3\CMS\Core\Type\ContextualFeedbackSeverity;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Extbase\Annotation\IgnoreValidation;
 use TYPO3\CMS\Extbase\Pagination\QueryResultPaginator;
 use TYPO3\CMS\Extbase\Persistence\Generic\PersistenceManager;
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;