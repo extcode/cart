513d973b431bfdc66d4070d242abb0b0dc0b376e
Gerald R <44193208+rintisch@users.noreply.github.com>
2024-04-02 22:01:19 +0200

    [TASK] Readd and fix BE list and order actions (#463)
    * [TASK] Readd BE actions for orders
    
    The way to add actions in the docheader changed
    with https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/11.5/Deprecation-95164-ExtbackendBackendTemplateView.html
    which needs adaptions.
    
    Fixes also the CSV export.
    
    This also fixes the issue that numbers could
    not be generated because `!isset('')` returns
    false.
    
    * [TASK] Code improvements after review
    
    Adaptions in `OrderController`:
    * Add searchArguments to export,
    * Follow CGL
    * Remove switch codeblock.

--- Classes/Controller/Backend/Order/OrderController.php
+++ Classes/Controller/Backend/Order/OrderController.php
@@ -10,15 +10,19 @@ namespace Extcode\Cart\Controller\Backend\Order;
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
  */
+
 use Extcode\Cart\Controller\Backend\ActionController;
 use Extcode\Cart\Domain\Model\Cart\Cart;
 use Extcode\Cart\Domain\Model\Order\Item;
 use Extcode\Cart\Domain\Repository\Order\ItemRepository;
 use Extcode\Cart\Event\Order\NumberGeneratorEvent;
 use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Backend\Template\Components\ButtonBar;
 use TYPO3\CMS\Backend\Template\ModuleTemplate;
 use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
+use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\Pagination\SimplePagination;
 use TYPO3\CMS\Core\Type\ContextualFeedbackSeverity;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
@@ -29,6 +33,8 @@ use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 
 class OrderController extends ActionController
 {
+    private const LANG_FILE = 'LLL:EXT:cart/Resources/Private/Language/locallang.xlf:';
+
     protected PersistenceManager $persistenceManager;
 
     private ModuleTemplate $moduleTemplate;
@@ -65,6 +71,8 @@ class OrderController extends ActionController
     {
         $this->moduleTemplate = $this->moduleTemplateFactory->create($this->request);
 
+        $this->setDocHeader($this->getListButtons());
+
         $this->moduleTemplate->assign('settings', $this->settings);
         $this->moduleTemplate->assign('searchArguments', $this->searchArguments);
 
@@ -98,7 +106,6 @@ class OrderController extends ActionController
     public function exportAction(): ResponseInterface
     {
         $format = $this->request->getFormat();
-
         $orderItems = $this->itemRepository->findAll($this->searchArguments);
 
         $this->view->assign('searchArguments', $this->searchArguments);
@@ -110,12 +117,11 @@ class OrderController extends ActionController
         $title = 'Order-Export-' . date('Y-m-d_H-i');
         $filename = $title . '.' . $format;
 
-        $this->responseFactory->createResponse()
+        return $this->responseFactory->createResponse()
             ->withAddedHeader('Content-Type', 'text/' . $format)
             ->withAddedHeader('Content-Description', 'File transfer')
-            ->withAddedHeader('Content-Disposition', 'attachment; filename="' . $filename . '"');
-
-        return $this->htmlResponse(null);
+            ->withAddedHeader('Content-Disposition', 'attachment; filename="' . $filename . '"')
+            ->withBody($this->streamFactory->createStream($this->view->render()));
     }
 
     /**
@@ -124,6 +130,8 @@ class OrderController extends ActionController
     public function showAction(Item $orderItem): ResponseInterface
     {
         $this->moduleTemplate = $this->moduleTemplateFactory->create($this->request);
+        $buttons = $this->getOrderButtons($orderItem);
+        $this->setDocHeader($buttons);
 
         $this->moduleTemplate->assign('settings', $this->settings);
         $this->moduleTemplate->assign('orderItem', $orderItem);
@@ -154,7 +162,25 @@ class OrderController extends ActionController
         return $this->moduleTemplate->renderResponse('Show');
     }
 
-    public function generateNumberAction(Item $orderItem, string $numberType): void
+    private function setDocHeader(array $buttons): void
+    {
+        $buttonBar = $this->moduleTemplate->getDocHeaderComponent()->getButtonBar();
+
+        foreach ($buttons as $button) {
+            $title = $this->getLanguageService()->sL(self::LANG_FILE . $button['title']);
+            $icon = $this->iconFactory->getIcon($button['icon'], Icon::SIZE_SMALL);
+
+            $viewButton = $buttonBar->makeLinkButton()
+                ->setHref($button['link'])
+                ->setTitle($title)
+                ->setShowLabelText($button['showLabel'])
+                ->setIcon($icon);
+            $buttonBar->addButton($viewButton, ButtonBar::BUTTON_POSITION_LEFT, $button['group']);
+        }
+
+    }
+
+    public function generateNumberAction(Item $orderItem, string $numberType): ResponseInterface
     {
         $getNumber = 'get' . ucfirst($numberType) . 'Number';
 
@@ -182,7 +208,7 @@ class OrderController extends ActionController
             $this->addFlashMessage($msg, '', ContextualFeedbackSeverity::ERROR);
         }
 
-        $this->redirect('show', 'Backend\Order\Order', null, ['orderItem' => $orderItem]);
+        return $this->redirect('show', 'Backend\Order\Order', null, ['orderItem' => $orderItem]);
     }
 
     public function getPaymentStatus(): array
@@ -233,5 +259,105 @@ class OrderController extends ActionController
             $shippingStatusArray[] = $shippingStatus;
         }
         return $shippingStatusArray;
+    }
+
+    protected function getLanguageService(): LanguageService
+    {
+        return $GLOBALS['LANG'];
+    }
+
+    private function getOrderButtons(Item $orderItem): array
+    {
+        $buttons = [
+            [
+                'link' => $this->uriBuilder->reset()->setRequest($this->request)
+                    ->uriFor(
+                        'list'
+                    ),
+                'title' => 'tx_cart.controller.order.action.close',
+                'icon' => 'actions-close',
+                'group' => 1,
+                'showLabel' => true,
+            ],
+        ];
+
+        $buttons = array_merge($buttons, $this->getDocumentButtons($orderItem, 'order', 2));
+        $buttons = array_merge($buttons, $this->getDocumentButtons($orderItem, 'invoice', 3));
+        $buttons = array_merge($buttons, $this->getDocumentButtons($orderItem, 'delivery', 4));
+
+        return $buttons;
+    }
+
+    private function getDocumentButtons(Item $orderItem, string $type, int $groupId): array
+    {
+        $buttons = [];
+
+        $numberGetter = 'get' . ucfirst($type) . 'Number';
+        $documentGetter = 'get' . ucfirst($type) . 'Pdfs';
+
+        $numberExists = $orderItem->$numberGetter();
+        $documentExists = $orderItem->$documentGetter()->current();
+
+        if (!$numberExists) {
+            $buttons[] = [
+                'link' => $this->uriBuilder->reset()->setRequest($this->request)
+                    ->uriFor(
+                        'generateNumber',
+                        ['orderItem' => $orderItem, 'numberType' => $type]
+                    ),
+                'title' => 'tx_cart.controller.order.action.generate' . ucfirst($type) . 'Number',
+                'icon' => 'actions-duplicates',
+                'group' => $groupId,
+                'showLabel' => true,
+            ];
+        }
+
+        if ($numberExists && ExtensionManagementUtility::isLoaded('cart_pdf')) {
+            $buttons[] = [
+                'link' => $this->uriBuilder->reset()->setRequest($this->request)
+                    ->uriFor(
+                        'create',
+                        ['orderItem' => $orderItem, 'pdfType' => $type],
+                        'Backend\Order\Document'
+                    ),
+                'title' => 'tx_cart.controller.order.action.generate' . ucfirst($type) . 'Document',
+                'icon' => 'actions-file-pdf',
+                'group' => $groupId,
+                'showLabel' => true,
+            ];
+        }
+
+        if ($documentExists) {
+            $buttons[] = [
+                'link' => $this->uriBuilder->reset()->setRequest($this->request)
+                    ->uriFor(
+                        'download',
+                        ['orderItem' => $orderItem, 'pdfType' => $type],
+                        'Backend\Order\Document'
+                    ),
+                'title' => 'tx_cart.controller.order.action.download' . ucfirst($type) . 'Document',
+                'icon' => 'actions-file-t3d-download',
+                'group' => $groupId,
+                'showLabel' => true,
+            ];
+        }
+
+        return $buttons;
+    }
+
+    private function getListButtons(): array
+    {
+        return [
+            [
+                'link' => $this->uriBuilder->reset()->setRequest($this->request)
+                    ->setArguments(['searchArguments' => $this->searchArguments])
+                    ->setFormat('csv')
+                    ->uriFor('export'),
+                'title' => 'tx_cart.controller.order.action.export.csv',
+                'icon' => 'actions-file-csv-download',
+                'group' => 1,
+                'showLabel' => true,
+            ],
+        ];
     }
 }
