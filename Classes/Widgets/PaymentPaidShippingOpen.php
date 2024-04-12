005921db6be9face1cdfd4c20fdf38f3dc3823ee
rintisch <gerald.rintisch@posteo.de>
2024-04-12 20:20:05 +0200

    [BUGFIX] Fix Pay-Shipping-Widget
    * Rename template due to recommended namespacing.
    * Remove deprecated StandaloneView
    * Add `page.tsconfig` for template path definition.

--- Classes/Widgets/PaymentPaidShippingOpen.php
+++ Classes/Widgets/PaymentPaidShippingOpen.php
@@ -11,60 +11,51 @@ namespace Extcode\Cart\Widgets;
  * LICENSE file that was distributed with this source code.
  */
 
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Backend\View\BackendViewFactory;
 use TYPO3\CMS\Dashboard\Widgets\ListDataProviderInterface;
+use TYPO3\CMS\Dashboard\Widgets\RequestAwareWidgetInterface;
 use TYPO3\CMS\Dashboard\Widgets\WidgetConfigurationInterface;
 use TYPO3\CMS\Dashboard\Widgets\WidgetInterface;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 
-class PaymentPaidShippingOpen implements WidgetInterface
+class PaymentPaidShippingOpen implements WidgetInterface, RequestAwareWidgetInterface
 {
-    /**
-     * @var WidgetConfigurationInterface
-     */
-    private $configuration;
-
-    /**
-     * @var ListDataProviderInterface
-     */
-    private $dataProvider;
-
-    /**
-     * @var StandaloneView
-     */
-    private $view;
-
-    /**
-     * @var array
-     */
-    private $options;
+    private WidgetConfigurationInterface $configuration;
+    private ListDataProviderInterface $dataProvider;
+    private array $options;
+    private BackendViewFactory $backendViewFactory;
+    private ServerRequestInterface $request;
+    private StandaloneView|\TYPO3\CMS\Core\View\ViewInterface $view;
 
     public function __construct(
         WidgetConfigurationInterface $configuration,
         ListDataProviderInterface $dataProvider,
-        StandaloneView $view,
+        BackendViewFactory $backendViewFactory,
         array $options = []
     ) {
         $this->configuration = $configuration;
         $this->dataProvider = $dataProvider;
-        $this->view = $view;
-        $this->options = array_merge(
-            [
-                'template' => 'Widget/PaymentPaidShippingOpen',
-            ],
-            $options
-        );
+        $this->backendViewFactory = $backendViewFactory;
+        $this->options = $options;
+    }
+
+    public function setRequest(ServerRequestInterface $request): void
+    {
+        $this->request = $request;
     }
 
     public function renderWidgetContent(): string
     {
-        $this->view->setTemplate($this->options['template']);
+        $this->view = $this->backendViewFactory->create($this->request);
 
         $this->view->assignMultiple([
             'items' => $this->dataProvider->getItems(),
             'options' => $this->options,
             'configuration' => $this->configuration,
         ]);
-        return $this->view->render();
+
+        return $this->view->render('Widget/CartPaymentPaidShippingOpen');
     }
     public function getOptions(): array
     {
