fc3281f905c7ba0fe547ccfb34c8eb8152834a7e
Gerald R <44193208+rintisch@users.noreply.github.com>
2024-05-22 09:01:14 +0200

    [BUGFIX] Fix dashboard widgets for v12 (#2)
    * [BUG] Fix BarChartWidgets in v12
    
    BarChartWidgets need a BackendViewFactory
    which is added now.
    
    * [BUG] Fix Pay-Shipping-Widget
    
    * Rename template due to recommended namespacing.
    * Remove deprecated StandaloneView
    * Add `page.tsconfig` for template path definition.
    
    * [TASK] Add types to properties of widget provider

--- Configuration/Backend/Widgets/PaymentPaidShippingOpenWidget.php
+++ Configuration/Backend/Widgets/PaymentPaidShippingOpenWidget.php
@@ -7,14 +7,15 @@ namespace Extcode\Cart\Configuration\Backend\Widget;
 use Extcode\Cart\Widgets\PaymentPaidShippingOpen;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\DependencyInjection\Reference;
+use TYPO3\CMS\Backend\View\BackendViewFactory;
 
 return function (ContainerConfigurator $configurator) {
     $services = $configurator->services();
 
     $services->set('dashboard.widget.extcode.cart.payment_paid_shipping_open')
         ->class(PaymentPaidShippingOpen::class)
+        ->arg('$backendViewFactory', new Reference(BackendViewFactory::class))
         ->arg('$dataProvider', new Reference('extcode.cart.provider.payment_paid_shipping_open'))
-        ->arg('$view', new Reference('dashboard.views.widget'))
         ->tag('dashboard.widget', [
             'identifier' => 'PaymentPaidShippingOpen',
             'groupNames' => 'cart',
