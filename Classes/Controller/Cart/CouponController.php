71a252984e6cf6525722eefe78f43f6d34b416f1
Gerald R <44193208+rintisch@users.noreply.github.com>
2024-05-30 12:58:09 +0200

    [BUGFIX] Entering coupon without errors (#499)
    * [BUGFIX] Entering coupon without errors
    
    * Redirect to cart after entering a coupon.
    * Make it possible to enter startdate and
      enddate for coupons in the backend.
    * Add `getIsUseable()` in coupon model to
      allow usage of {coupon.isUseable} in
      Fluid.
    
    Fixes #498
    
    * [BUGFIX] Enable removing of coupon from cart
    
    After removing a coupon from the cart the cart
    shall only be reloaded instead of responding with
    an own htmlResponse().
    
    * [BUGFIX] Use `datetime` instead of only `date`
    
    * [BUGFIX] Remove superfluous method
    
    Previous commit introduced a superfluous method
    `getIsUseable()` which is not necessary because
    `isUseable` works as well with fluid for
    `{coupon.useable}`.

--- Classes/Controller/Cart/CouponController.php
+++ Classes/Controller/Cart/CouponController.php
@@ -121,8 +121,6 @@ class CouponController extends ActionController
             }
 
             $this->sessionHandler->writeCart($this->settings['cart']['pid'], $this->cart);
-
-            return $this->htmlResponse();
         }
 
         return $this->redirect('show', 'Cart\Cart');
@@ -159,8 +157,6 @@ class CouponController extends ActionController
             }
 
             $this->sessionHandler->writeCart($this->settings['cart']['pid'], $this->cart);
-
-            return $this->htmlResponse();
         }
 
         return $this->redirect('show', 'Cart\Cart');
