4fdc822ae358fac96c49182ce514bc459e97314d
Daniel Gohlke <daniel.gohlke@extco.de>
2024-09-30 09:17:59 +0200

    [BUGFIX] Use explicit keys in items configuration

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -6,19 +6,19 @@ call_user_func(function () {
     $_LLL_db = 'LLL:EXT:cart/Resources/Private/Language/locallang_db.xlf:';
 
     $GLOBALS['TCA']['pages']['columns']['doktype']['config']['items'][] = [
-        $_LLL_db . 'pages.doktype.181',
-        181,
-        'apps-pagetree-page-cart-cart',
+        'label' => $_LLL_db . 'pages.doktype.181',
+        'value' => 181,
+        'icon' => 'apps-pagetree-page-cart-cart',
     ];
     $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [
-        $_LLL_db . 'tcarecords-pages-contains.cart_coupons',
-        'coupons',
-        'apps-pagetree-folder-cart-coupons',
+        'label' => $_LLL_db . 'tcarecords-pages-contains.cart_coupons',
+        'value' => 'coupons',
+        'icon' => 'apps-pagetree-folder-cart-coupons',
     ];
     $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [
-        $_LLL_db . 'tcarecords-pages-contains.cart_orders',
-        'orders',
-        'apps-pagetree-folder-cart-orders',
+        'label' => $_LLL_db . 'tcarecords-pages-contains.cart_orders',
+        'value' => 'orders',
+        'icon' => 'apps-pagetree-folder-cart-orders',
     ];
 
     $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes'][181] = 'apps-pagetree-page-cart-cart';
